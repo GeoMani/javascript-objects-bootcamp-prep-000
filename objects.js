2ab4f0ffacb6d928004288c7854dd48dcc4337f4
George Samuel Igoe <george.igoe@gmail.com>
2018-06-13 20:42:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = { Nas: "One Mic"}
 
 function updatePlaylist (object, artistName, songTitle) {
@@ -10,4 +11,10 @@ function removeFromPlaylist (playlist, artistName) {
   delete playlist[artistName];
   
   return playlist;
+=======
+var playlist = {name: "song title"}
+
+function updatePlaylist (object, name, songtitle) {
+  object.name = 'songtitle';
+>>>>>>> 8219c8cc3384b5430b32d7540b461086273abe7d
 }
